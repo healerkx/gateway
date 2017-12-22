1035ecbbe30f84b8f32867c8e66b98174253f9d5
healerkx <healer.kx.yu@gmail.com>
2017-12-22 19:59:41 +0800

    Log content in file with  date-name

--- src/middleware/logger.go
+++ src/middleware/logger.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
-	
+	"fmt"
+	"os"
+	"time"
 )
 
 type Logger struct {
@@ -14,28 +16,62 @@ func NewLogger() *Logger {
 	}
 }
 
-type LogContent struct {
-	url string;
+
+type LogFile struct {
+	file 		*os.File;
+	fileName 	string;
 }
 
-func NewLogContent(url string) LogContent {
-	return LogContent {
-		url: url,
+func NewLogFile() *LogFile {
+	return &LogFile{} 
+}
+
+func getFileName(groupId int32, dateTime time.Time) string {
+	return fmt.Sprintf("%d_%s.log", groupId, dateTime.Format("2006-01-02"))
+}
+
+func (this *LogFile) WriteContent(groupId int32, content string) {
+	now := time.Now()
+	if this.file != nil {
+		// TODO: If expired, switch file to new
+		if this.fileName != getFileName(groupId, now) {
+			this.file.Close()
+			this.file = nil
+		}
 	}
+
+	if this.file == nil {
+		fileName := getFileName(groupId, now)
+		var err error
+		if this.file, err = os.OpenFile(fileName, os.O_CREATE | os.O_APPEND, 0666); err == nil {
+			this.fileName = fileName
+			this.file.WriteString(content + "\n")
+			this.file.Sync()
+		}
+	}
+
+
 }
 
 
-var gLoggerChannel = make(chan LogContent, 10)
+var gLoggerChannel = make(chan *RequestHolder, 10)
 
-func logFileContent(content LogContent) {
+var gLogFileMap = make(map[int32]*LogFile)
 
+func logFileContent(requestHolder *RequestHolder) {
+	logFile := gLogFileMap[requestHolder.GroupId]
+	if logFile == nil {
+		logFile = NewLogFile()
+		gLogFileMap[requestHolder.GroupId] = logFile
+	}
+	logFile.WriteContent(requestHolder.GroupId, requestHolder.Url)
 }
 
 func DumpLog() {
 	for {
 		select {
-		case msg := <-gLoggerChannel:
-			logFileContent(msg)
+		case rh := <-gLoggerChannel:
+			logFileContent(rh)
 			// fmt.Printf("LOG: %s;", msg)
 		//default:
 		//	time.Sleep(0)
@@ -46,6 +82,6 @@ func DumpLog() {
 
 func (this *Logger) Handle(requestHolder *RequestHolder) bool {
 	// TODO: Send content to a goroutine.
-	gLoggerChannel <- NewLogContent(requestHolder.Url)
+	gLoggerChannel <- requestHolder
 	return true
 }
